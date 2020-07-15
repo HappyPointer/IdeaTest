ca4c4ace2ab514ebd242849e6cf453dd5fe6cef8
HappyPointer <1063650139@qq.com>
2020-07-15 14:39:17 +0800

    New Idea test commit

---
+++ src/DefaultPackage/TestDefault.java
@@ -0,0 +1,17 @@
+package DefaultPackage;
+
+public class
+TestDefault {
+
+    public static void main(String[] args){
+
+
+        System.out.println("This is the first test");
+        System.out.println("This is the second test");
+
+
+    }
+
+
+
+}
